8fd903b88ff7bcd4fad1db11985a5c4d69c5a79e
tomams <tomams@tomass-mbp.fairfield.edu>
2024-02-14 15:24:37 -0500

    Not working yet

--- queue/QueueImpl.java
+++ queue/QueueImpl.java
@@ -1,41 +1,78 @@
 package queue;
 
 public class QueueImpl implements Queue {
+	
+	  protected final int DEFCAP = 20; // default capacity
+	  protected String[] elements;           // array that holds queue elements
+	  protected int size = 0;    // number of elements in this queue
+	  protected int front = 0;          // index of front of queue
+	  protected int rear;               // index of rear of queue
 
+	  
+	  public QueueImpl(int maxSize) 
+	  {
+		this.elements = new String[maxSize];
+	    rear = maxSize - 1;
+	  }
+	  
 	@Override
 	public boolean isFull() {
 		// TODO Auto-generated method stub
-		return false;
+		return (size == elements.length);
 	}
 
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
-		return false;
+		return (size == 0);
 	}
 
 	@Override
 	public void enQueue(String element) {
 		// TODO Auto-generated method stub
 		
+		if (isFull())
+		      System.err.println("Queue is full.");
+		    else {
+		      rear = (rear + 1) % elements.length;
+		      elements[rear] = element;
+		      size ++;
+		      System.out.println("Added element to queue.");
+		    }
+		
 	}
 
 	@Override
 	public String deQueue() {
 		// TODO Auto-generated method stub
+		if (isEmpty())
+		     System.err.println("Couldn't remove element from empty queue.");
+		else {
+		      String toReturn = elements[front];
+		      elements[front] = null;
+		      front = (front + 1) % elements.length;
+		      size --;
+		      System.out.println("Removed element from queue");
+		      return toReturn;
+		    }
 		return null;
+		
 	}
 
 	@Override
 	public void display() {
 		// TODO Auto-generated method stub
+		System.out.println("Elements in the list: ");
+		for(int i = 0; i<=size; i++) {
+			System.out.println(elements[i]);
+		}
 		
 	}
 
 	@Override
 	public String peek() {
 		// TODO Auto-generated method stub
-		return null;
+		return elements[rear];
 	}
 
 }
--- queue/QueueTester.java
+++ queue/QueueTester.java
@@ -1,9 +1,35 @@
 package queue;
+import java.util.Scanner;
 
 public class QueueTester {
 
 	public static void main(String[] args) {
-
+		
+		Queue tester = new QueueImpl(20);
+		
+		String name = "placeholder";
+		Scanner scanner = new Scanner(System.in);
+		int length = name.length();
+		
+		System.out.print("Enter a rider's name, or leave the name blank to quit: ");
+		while(length == 0) {
+			
+			name = scanner.nextLine();
+			
+			length = name.length();
+			
+			tester.enQueue(name);
+			if(length == 0) {
+				System.out.print("Enter another rider's name, or leave the name blank to quit: ");
+			}
+			else {
+				System.out.println("Quitting..");
+			}
+		}
+		
+		
+		tester.peek();
+		tester.display();
 	}
 
 }
