76938441223a04ee575282870e4bc10b7e2ac40e
SteveB519 <stephenbor175@gmail.com>
2024-02-14 15:12:03 -0500

    Merge branch 'master' of git@github.com:jkramerDev/Lab1_group5.git

--- stack/StackImpl.java
+++ stack/StackImpl.java
@@ -1,54 +1,114 @@
 package stack; 
 
 public class StackImpl implements Stack {
-
-	@Override
-	public void push(String s) {
 	
-		System.out.println("thing i added to stack: " + s);
+	private String[] elements;
+	private int top;
+	private int size;
+	
+	public StackImpl(int size)
+	{
+		elements = new String[size];
+		top = -1;
+		this.size=size;
+	}
+	
+	@Override
+	public void push(String s) 
+	{
+		
+		//Increases top location
+		if (!isFull())
+		{
+			top++;
+		
+			//assigns element to top
+			elements[top] = s;
+		}
+		else
+			System.out.println("Cannot add String: "+ s+ " because stack is full");
+		//for testing push method
+		//System.out.println("thing i added to stack: " + s);
 	
 	}
 
 	@Override
-	public String pop() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Boolean isFull() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public String peek() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setCapacity(int size) {
-		// TODO Auto-generated method stub
+	public String pop() 
+	{
+		
+		if (top != -1)
+		{
+			String s = elements[top];
+			top--;
+			return s;
+		}
+		else
+		{
+			System.out.println("Stack is empty, nothing to pop");
+			return null;
+		}
 		
 	}
 
 	@Override
-	public void display() {
-		// TODO Auto-generated method stub
+	public Boolean isEmpty() 
+	{
 		
+		//Checks to see if top is equal to -1 when initialized
+		return top==-1;
+
+	}
+
+	@Override
+	public Boolean isFull() 
+	{
+		
+		//Checks to see if top is equal to size
+		//size is minus 1 due to indexing
+		return top == size-1;
+	}
+
+	@Override
+	public int size() 
+	{
+		
+		//Returns the total number of items in an array
+		return top+1;
+	}
+
+	@Override
+	public String peek() 
+	{
+		
+		return elements[top];
+	}
+
+	
+	/*
+	Said to ignore
+	capacity is set when stack is initialized in constructor 
+	Changed to get capacity
+	 */
+	@Override
+	public int getCapacity() 
+	{
+		return size;
+	}
+	
+	
+	@Override
+	public void display() 
+	{
+		if (!isEmpty())
+		{
+			System.out.println("The Strings in the stack are: ");
+			for(int i = 0; i<=top;i++)
+			{
+				System.out.println(elements[i]);
+			}
+		}
+		else
+			System.out.println("The stack is empty");
 	}
  
 
--- stack/StackTester.java
+++ stack/StackTester.java
@@ -4,13 +4,98 @@ public class StackTester {
 
 	public static void main(String[] args) {
 
-		Stack stackTester = new StackImpl();
-		stackTester.push("thing in stack");
+		Stack stackTester = new StackImpl(10);
+		runTests(stackTester);
+		
 
 	}
 	
-	public static void runTests(Stack stack) {
-	 	
+	public static void runTests(Stack stack) 
+	{
+		//Check is stack empty
+		System.out.println("Is stack Empty: " + stack.isEmpty());
+		
+		//Gets number of elements in stack
+		System.out.println("Number of strings in stack: "+ stack.size());
+		
+		//Pushes 2 names to stack
+	 	stack.push("John");
+		stack.push("Jake");
+		
+		//Gets number of elements in stack
+		System.out.println("Number of strings in stack: "+ stack.size());
+				
+		//Pushes 3 more names to stack
+		stack.push("Jack");
+		stack.push("Matt");
+		stack.push("Luke");
+		
+		//Displays items in stack
+		stack.display();
+		
+		//Peeks top item
+		System.out.println("Top item in stack: " + stack.peek());
+		
+		//pops top two
+		System.out.println("Popped item is: "+stack.pop());
+		System.out.println("Popped item is: "+stack.pop());
+		
+		//Gets number of elements in stack
+		System.out.println("Number of strings in stack: "+ stack.size());
+		
+		//Check is stack empty and full
+		System.out.println("Is stack Empty: " + stack.isEmpty());
+		System.out.println("Is stack Full: " + stack.isFull());
+		
+		//Displays items in stack
+		stack.display();
+		
+		//Peeks top item
+		System.out.println("Top item in stack: " + stack.peek());
+		
+		//Push lots of items into stack more then set capacity
+		stack.push("A");
+		stack.push("B");
+		stack.push("C");
+		stack.push("D");
+		stack.push("E");
+		stack.push("F");
+		stack.push("G");
+		
+		//Check is stack empty and full
+		System.out.println("Is stack Empty: " + stack.isEmpty());
+		System.out.println("Is stack Full: " + stack.isFull());
+		stack.push("H");
+		stack.push("I");
+		stack.push("J");
+		stack.push("K");
+		
+		//Check is stack empty and full
+		System.out.println("Is stack Empty: " + stack.isEmpty());
+		System.out.println("Is stack Full: " + stack.isFull());
+		
+		//Check capacity of the stack
+		System.out.println("Total stack capacity: " + stack.getCapacity());
+		
+		//Pops all and more elements then inserted
+		System.out.println("Popped item is: "+stack.pop());
+		System.out.println("Popped item is: "+stack.pop());
+		System.out.println("Popped item is: "+stack.pop());
+		System.out.println("Popped item is: "+stack.pop());
+		System.out.println("Popped item is: "+stack.pop());
+		System.out.println("Popped item is: "+stack.pop());
+		System.out.println("Popped item is: "+stack.pop());
+		System.out.println("Popped item is: "+stack.pop());
+		System.out.println("Popped item is: "+stack.pop());
+		System.out.println("Popped item is: "+stack.pop());
+		
+		//Elements that do not exist
+		System.out.println("Popped item is: "+stack.pop());
+		System.out.println("Popped item is: "+stack.pop());
+		
+		//Displaying empty stack
+		stack.display();
+		
 	}
 
 }
